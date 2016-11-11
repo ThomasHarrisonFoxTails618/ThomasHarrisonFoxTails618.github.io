7df6c8027c9d3c8d926706705aa712b451910151
ThomasHarrisonFoxTails618 <18harrisonet2@wis.edu.hk>
2016-11-11 14:40:00 +0800

    upload game

---
+++ Thebridge/js/index.js
@@ -0,0 +1,183 @@
+function game() {
+var setep1 = prompt ("You became an employee on a secret lab on an island, and one day a secret experement was releced, zombies are now reliced and you along with all of the employees managed to escape, but now there is a problem. There is a rope bridge ahead of you, and it can only hold 2 people at the time, plus it is in the middle of the night, and you managed to bring one latern, wich lights up a small area. There are 4 people (including yourself), you can go acrros the bridge in 1 minute, the young inventor takes 2 minutes, the janitor takes 5, while the old proffesor takes 10. When people go 2 at a time, the time taken is how fast the slowest can go, and if 2 people go across, another has to go back to pick up the other people left on the other side. The zombies will arive in 17 minutes, and you need to find a way to safley cross all 4 people, and cut the ropes. Which 2 goes first? Y= you, I= inventor, J= janitor, P= prof. (write the letters in alebetical order)");
+
+if (setep1 === "JP"){
+  var step88 = prompt ("The Janitor and the Prof goes across, there is now 7 minutes left. Who goes across next? The Prof (P), or the Jan");
+}
+if (step88 === "P"){
+  var step842 = prompt ("The Prof goes across but time ran out. GAME OVER!!!!!");
+}
+if (step88 === "J"){
+  var step248 = prompt ("The Janitor goes across the bridge there is 2 minutes left");
+}
+if (setep1 === "IJ"){
+  var step45 = prompt ("The Inventor and the Janitor, goes across, taking 5 minutes, there is now 12 minuts left, who goes across next? The Inventor or the Janitor?");
+}
+if (step45 === "J"){
+  var step419 = prompt("The Janitor walks down the bridge, there is now 7 minutes left. Who goes across next? You and the Janitor (JY), You and the Prof (PY) or the Janitor and the Prof (JP)?");
+}
+if (step419 === "JP"){
+  var step123 = prompt("The Janitor and the Prof goes across the bridge, but time ran out. GAME OVER!!!!!!!");
+}
+if (step419 === "YJ"){
+  var step4191 = prompt("You and the Janitor goes across the bridge, there is now 2 minutes left. who goes across next? You (Y), the Janitor (J) or the Inventor(I)?");
+}
+if (step4191 === "Y"){
+  var step4192 = prompt ("You go across to pick up the prof, but time ran out! GAME OVER!!!!");
+}
+if (step4191 === "I"){
+  var step4192 = prompt ("The Inventor goes across to pick up the prof, but time ran out! GAME OVER!!!!");
+}
+if (step4191 === "J"){
+  var step4192 = prompt ("The Janitor goes across to pick up the prof, but time ran out! GAME OVER!!!!");
+}
+if (step45 === "I"){
+  var step44 = prompt ("The Inventor goes across the bridge in 2 minutes, there is now 10 minutes left. Who goes across next? You and the Inventor (IY), You and the Prof (PY), or the Inventor and the Prof (IP)?");
+}
+if (step44 === "PY"){
+  var step401 = prompt ("You and the Prof goes across, but time ran out. GAME OVER!!!!");
+}
+if (step44 === "IP"){
+  var step411 = prompt ("The Inventor and the prof goes across, but time ran out. GAME OVER!!!!!!!!!");
+}
+if (step44 === "IY"){
+  var step46 = prompt ("You and the Inventor goes across the bridge, there is now 8 minutes left. Who goes across next? You, the Inventor or the Janitor?");
+}
+if (step46 === "I"){
+  var step47 = prompt ("The inventor goes across the bridge, there is now 6 minutes left! He takes the Prof, but time ran out. GAME OVER!!");
+}
+if (step46 === "Y"){
+  var step47 = prompt ("You go across the bridge, there is now 7 minutes left! You take the Prof, but time ran out. GAME OVER!!");
+}
+if (step46 === "J"){
+  var step47 = prompt ("The Janitor go across the bridge, there is now 2 minutes left! He takes the Prof, but time ran out. GAME OVER!!");
+}
+
+  if (step2 === "I") {
+    var step03 = prompt ("The Inventor goes across the bridge, there is only 13 minutes left. Who goes next? The Inventor and Janitor? (IJ) or the Inventor and Prof (IP) or the Prof and Janitor (JP)");
+    }
+if (step03 === "JP"){
+  var step042 = prompt ("The Janitor and Prof goes across, there is 3 minutes left. Who goes across next? You(Y), the Prof (P) or the Janitor (J).");
+}
+if (step042 === 'P'){
+  var step042 = prompt ("The Prof goes across, but time ran out. GAME OVER!!!!!");
+}
+if (step042 === 'J'){
+  var step042 = prompt ("The Janitor goes across, but time ran out. GAME OVER!!!!!");
+}
+if (setep1 === "IY") {
+  var step2 = prompt ("You and the inventor safley cross the bridge in 2 minutes, there are now 15 minutes left, now who goes across next? You, or the Inventor? (for you type 'Y' or for the Inventor type 'I', ");
+}
+if (step042 === 'Y'){
+  var step042 = prompt ("You go across, now you go across with the Inventor, and you make it out in the nick of time!");
+}
+if (step03 === "IJ"){
+  var step04 = prompt ("The Inventor and Janitor, goes across the bridge, there is now 8 minutes left, who goes across next? You (Y), The Inventor (I), or the Janitor (J)?");
+}
+if (step03 === "IP"){
+  var step041 = prompt ("The Inventor and Prof, goes across the bridge, there is now 3 minutes left! Who goes across next? You (Y), The Inventor (I), or the Prof(P)?");
+}
+if (step041 === "Y"){
+  var step0412 = prompt ("You go across the bridge to pick up the Janitor but time ran out. GAME OVER!!!!!!");
+}
+if (step041 === "I"){
+  var step0412 = prompt ("The Inventor goes across the bridge to pick up the Janitor but time ran out. GAME OVER!!!!!!");
+}
+if (step041 === "P"){
+  var step0412 = prompt ("The Prof goes across the bridge to pick up the Janitor but time ran out. GAME OVER!!!!!!");
+}
+if (step04 === "Y"){
+  var step05 = prompt ("You go across the bridge and there is now 7 minutes left, you go with the prof but TIME RAN OUT! GAME OVER!!!!!");
+}
+if (step04 === "I"){
+  var step05 = prompt ("The Inventor goes across the bridge to pick up the prof, but it is too late!! GAME OVER!!!!");
+}
+if (step04 === "J"){
+  var step05 = prompt ("The Janitor goes across the bridge to pick up the prof, but it is too late!! GAME OVER!!!!");
+}
+  if (setep1 === "IJ"){
+    var step221 = prompt ("The Inventor and the prof, goes across the bridge, but there are now 7 minutes left! Who goes across next? The Prof or the Inventor (Prof=P, Inventor=I)");
+  }
+if (step221 === "I"){
+  var step222 = prompt ("The Inventor goes across the bridge, there is now 5 minutes left! Who goes next? (IY= Inventer and You, IJ= Inventor and Janitor, JY= Janitor and you.)");
+}
+if (step221 === "P"){
+  var step222 = prompt("The Prof hasnt finished crossing the bridge when the zombies arrived. GAME OVER!!!!!");
+}
+if (step222 === "IJ"){
+  var step223 = prompt ("The Inventor and Janitor goes across the bridge, but TIME IS ALREADY OUT!!!!! GAME OVER!");
+}
+if (step222 === "JY"){
+  var step223 = prompt ("The You and Janitor goes across the bridge, but TIME IS ALREADY OUT!!!!! GAME OVER!");
+}
+if (step222 === "IY"){
+  var step223 = prompt ("You and the Inventor go across the bridge in 2 minutes, there is now 3 minutes left! Who goes next? You (type Y), the Prof (type P) or the Inventor (type I)?");
+}
+if (step2 === "Y") {
+  var step3 = prompt ("You go across the bridge in 1 minute, there are now 14 minutes left. Now who goes across next? You and the Janitor? You and the Prof? or Prof and the Janitor? (You and the Janitor = 'JY', You and the Prof = 'PY' and the Janitor and the Prof = 'JP')");
+}
+
+if (step3 === "JP") {
+  var step4 = prompt ("The Prof is slow and it takes 10 minutes, and there are only 4 MINUTES LEFT! Who goes next!? The Prof, The Janitor, or the Inventor? (Prof = P, Janitor = J, Inventor = I)");
+}
+if (step4 === "I") {
+  var step5 = prompt ("The Inventor goes across the bridge and arrives, there are only 2 minutes left! But You and the Inventor can go across! type IY and you will win!");
+}
+if (step5 === "IY") {
+  var step6 = prompt ("You and the inventor safley go across the bridge and you cut the ropes! JUST IN THE NICK OF TIME! END! YOU WIN!!!!!!!!");
+}
+if (setep1 === "JY"){
+  var step20 = prompt ("You and the Janitor go across the brigde in 5 minutes, there is now 12 minutes left, WHO GOES NEXT? You or the Janitor?(You= Y, Janitor= J )");
+}
+  if (step20 === "Y"){
+  var step21 = prompt ("You go across the bridge and there are now 11 minutes left, WHO GOES NEXT? You, the Inventor, or the Prof? (You and Inventor = IY, Prof and Inventor = IP, You and Prof = PY)");
+  }
+if (step21 === "IP")
+  var step22 = prompt ("The Inventor and the Prof, goes across, BUT THERE IS ONLY 1 MINUTE LEFT. GAME OVER!!!!!!!");
+
+if (step21 === "IY") {
+  var step22 = prompt ("You and the Inventor go across the bridge in 2 minutes, there are now 9 minutes left! WHO GOES NEXT? You, the Inventor, and the Janitor. (You = Y, Inventor = I, Janitor = J)");
+
+ if (step21 === "PY")
+   var step22 = prompt ("You cross with the prof there is 1 MINUTE LEFT! You go across, but it is too late! GAME OVER!");
+}
+if (step22 === "Y") {
+  var step23 = prompt ("You go across there are now 8 minutes left, now you go across wit the prof, but the zombies beat you to it. GAME OVER!");
+}
+if (step22 === "J") {
+  var step23 = prompt ("the janitor goes across there are now 3 minutes left, now the janitor go across with the prof, but the zombies beat them to it. GAME OVER!");
+}
+if (step22 === "I") {
+  var step23 = prompt ("the Inventor goes across there are now 7 minutes left, now the inventor go across with the prof, but the zombies beat them to it. GAME OVER!");
+}
+  
+if (setep1 === "PY") {
+  var step20 = prompt ("You and the Prof go across the bridge in 10 minutes, there are 7 minutes left! WHO GOES NEXT!? you or the prof?");
+}
+if (step20 === "P") {
+ prompt ("The Prof goes across BUT THERE IS ONLY 1 MINUTE LEFT!!!!! GAME OVER!");
+}
+if (step20 === "Y") {
+  var step30 = prompt ("There are 6 minutes left when you go across, WHO GOES NEXT? You, the Inventor, or the Janitor? (YI= You and the inventor, YJ = You and the Janitor, IJ = Inventor and the Janitor)");
+}
+if (step30 === "YI"){
+  var step41 = prompt ("THERE IS 4 MINUTES LEFT! Who goes next? You (Y) or the Inventor (I), to get the Janitor?");
+}
+if (step41==="Y"){
+  var step412 = prompt ("You go across, but when you cross with the janitor, TIME RUNS OUT!!!!! GAME OVER!!!!");
+}
+if (step41==="I"){
+  var step412 = prompt ("The Inventor goes across, but when he crosses with the janitor, TIME RUNS OUT!!!!! GAME OVER!!!!");
+}
+if (step30 === "IJ") {
+ var step40 = prompt ("THERE ARE ONLY 2 MINUTES LEFT! WHO GOES NEXT? The Janitor, the Prof or the Inventor");
+}
+if (step40 === "I") {
+  prompt ("THE ZOMBIES ARE HERE GAME OVER!!!!!!");
+}
+if (step40 === "J") {
+  prompt ("THE ZOMBIES ARE HERE GAME OVER!!!!!!");
+}
+if (step40 === "P") {
+  prompt ("THE ZOMBIES ARE HERE GAME OVER!!!!!!");}
+}
